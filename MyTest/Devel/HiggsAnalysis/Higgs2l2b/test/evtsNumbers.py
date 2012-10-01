f8ecfc4bd592aab56c6dd54f3732773a73c4869d
decosa <>
2012-10-01 15:05:39 +0000

    added latex table as output

--- MyTest/Devel/HiggsAnalysis/Higgs2l2b/test/evtsNumbers.py
+++ MyTest/Devel/HiggsAnalysis/Higgs2l2b/test/evtsNumbers.py
@@ -4,6 +4,7 @@ sys.argv.append('-b')
 import ROOT
 
 from HiggsAnalysis.Higgs2l2b.scaleFactors import scale
+from matrix2latex import matrix2latex
 
 ROOT.gROOT.Reset();
 ROOT.gROOT.SetStyle('Plain')
@@ -14,10 +15,10 @@ ROOT.gROOT.SetBatch()
 ### Evaluate the integral between two points
 def integ(histo, lower, upper):
     bmin = histo.GetXaxis().FindBin(lower)
-    bmax = histo.GetXaxis().FindBin(lower)
+    bmax = histo.GetXaxis().FindBin(upper)
     integ = histo.Integral(bmin,bmax)
-    integ =-histo.GetBinContent(bmin)*(lower - histo.GetXaxis().GetBinLowEdge(bmin))/ histo.GetXaxis().GetBinWidth(bmin)
-    integ =-histo.GetBinContent(bmax)*(histo.GetXaxis().GetBinUpEdge(bmax) -upper)/ histo.GetXaxis().GetBinWidth(bmax)
+    integ -= (histo.GetBinContent(bmin)*(lower - histo.GetXaxis().GetBinLowEdge(bmin)))/ histo.GetXaxis().GetBinWidth(bmin)
+    integ -= (histo.GetBinContent(bmax)*(histo.GetXaxis().GetBinUpEdge(bmax) -upper))/ histo.GetXaxis().GetBinWidth(bmax)
     return integ
 
 
@@ -29,12 +30,14 @@ def evalNumEvts(histosBkg, histosSig, mass):
     signal = "GluGluToHToZZTo2L2Q_M-"+str(mass)+"_8TeV"
     nEvts = dict([(s,integ(h, low, up) ) for s, h in histosBkg.iteritems()])
     nEvts[signal] =integ(histosSig[signal], low, up)
+    return nEvts
+    #print nEvts
+    #print "MASS: ", str(mass)
+    #print "Event numbers in the mass window: [",str(low),", ",str(up), " ]"
 
-    print "MASS: ", str(mass)
-    print "Event numbers in the mass window: [",str(low),", ",str(up), " ]"
-    for (s, num) in nEvts.iteritems():
-        print s, ": ", str(num)
-        
+    #for (s, num) in nEvts.iteritems():
+        #print s, ": ", str(num)
+    #return nEvts
 ### Evaluate the number of events in a mass window for all mass points
 def evalNumEvtsMassWind(var, ch):
 
@@ -55,8 +58,29 @@ def evalNumEvtsMassWind(var, ch):
     print "Channel: ", ch
     print " "
     [evalNumEvts(bkg_hist, sig_hist, m) for m in masses]
+
     print "*"*40
 
+    hr = ['Mass [GeV]','Signal', '$Z$+Jets', '$tt$', 'ZZ/WZ/WW', 'TotalBkg']
+    hc = [str(m) for m in masses]
+    numbers = []
+    for m in masses:
+        signal = "GluGluToHToZZTo2L2Q_M-"+str(m)+"_8TeV"
+        print signal
+        nEvts = evalNumEvts(bkg_hist, sig_hist, m)
+        print nEvts
+        numbers.append([m,
+                        nEvts[signal],
+                        nEvts["DYJetsToLL_M-50"],
+                        nEvts["TT"],
+                        nEvts["ZZ"] + nEvts["WZ"] + nEvts["WW"],
+                        nEvts["DYJetsToLL_M-50"] + nEvts["TT"]+ nEvts["ZZ"] + nEvts["WZ"] + nEvts["WW"]
+                        ])
+
+    #t = matrix2latex(numbers, var+"_"+ch+"_evts.py",headerRow=hr)
+    t = matrix2latex(numbers, var+"_"+ch+"_evts.py", headerColumn=hc, headerRow=hr)
+
+    
 ### Evaluate the number of events in all the mass range
 def evalNumEvtsAllRange(var, ch):
     bkgSamples = [ "WW", "WZ", "ZZ", "TT", "DYJetsToLL_M-50"]
@@ -84,6 +108,7 @@ def evalNumEvtsAllRange(var, ch):
         print s, ": ", str(num)
     print "*"*40
 
+    
 
 
 
@@ -93,9 +118,9 @@ def evalNumEvtsAllRange(var, ch):
 ### channel = "El" or "Mu"
     
 ch = "El"
-evalNumEvtsAllRange("lljjmass", ch)
-evalNumEvtsAllRange("lljjmass_1btag", ch)
-evalNumEvtsAllRange("lljjmass_0btag", ch)
+#evalNumEvtsAllRange("lljjmass", ch)
+#evalNumEvtsAllRange("lljjmass_1btag", ch)
+#evalNumEvtsAllRange("lljjmass_0btag", ch)
 
 
 ### Call the function evalNumEvtsMassWind to get the number of events in the mass range [-6%, +10%]mH for signal and bkg
@@ -103,3 +128,7 @@ evalNumEvtsAllRange("lljjmass_0btag", ch)
 ### evalNumEvtsMassWind(category, channel)
 ### category = "lljjmass", "lljjmass_1btag" or "lljjmass_0btag"
 ### channel = "El" or "Mu"
+
+evalNumEvtsMassWind("lljjmass", ch)
+evalNumEvtsMassWind("lljjmass_1btag", ch)
+evalNumEvtsMassWind("lljjmass_0btag", ch)
